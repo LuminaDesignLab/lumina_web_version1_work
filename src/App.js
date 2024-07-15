0849e9d7ae5ccdb318326564abd0192c6875447f
joonho_park_lumina <davidpro90@gmail.com>
2024-07-16 00:58:15 +0900

    header nav

--- src/App.js
+++ src/App.js
@@ -11,10 +11,18 @@ function App() {
 function Header() {
   return (
     <header className="navbar_container">
-      <div className="navbar_items">
+      <div className="navbar_box">
         <div className="navbar_logo_container">
-          <span className="navbar_logo_title">루미나LUMINA </span>
-          <span className="navbar_logo_subtext">DesignLab</span>
+          <span className="navbar_logo_title">LUMINA </span>
+          <span className="navbar_logo_subtext">Design Lab</span>
+        </div>
+        <ul className="navbar_items">
+          <li className="navbar_item">Home</li>
+          <li className="navbar_item">About</li>
+          <li className="navbar_item">Contact</li>
+        </ul>
+        <div className="red_bubble">
+          <span>무료상담</span>
         </div>
       </div>
     </header>
