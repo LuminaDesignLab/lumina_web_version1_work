5b8af8abda7958e8cbf4702a4da8edc4b9edc3af
joonho_park_lumina <davidpro90@gmail.com>
2024-07-16 00:17:38 +0900

    google font link

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <header className="navbar_container">
+      <div className="navbar_items">
+        <div className="navbar_logo_container">
+          <span className="navbar_logo_title">루미나LUMINA </span>
+          <span className="navbar_logo_subtext">DesignLab</span>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default App;
